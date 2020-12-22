7b9d825bf58c7ae58781290b113f40664b8cca0e
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2020-12-22 17:30:37 +0500

    Task 2 comp

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+    System.out.println("----Task 1----");
 	int a[] = new int []{1,1,0, 0, 1, 0, 1, 1, 0, 0};
 	printArray(a);
     for (int i=0; i<a.length; i++){
@@ -8,7 +9,16 @@ public class Main {
         else a[i]=0;
     }
     printArray(a);
+    System.out.println("----Task 2----");
+    int b[] = new int[8];
+    for (int i=0; i<b.length; i++){
+        b[i] = i * 3;
     }
+    printArray(b);
+    System.out.println("----Task 3----");
+    }
+
+
     static void printArray (int b[]){
         for (int i=0; i<b.length; i++){
             System.out.print(b[i] + ", ");
