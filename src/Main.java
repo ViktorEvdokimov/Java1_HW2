3db05886cbaea6a8a82a24b633732ead2c4b8a17
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2020-12-22 17:24:36 +0500

    Task 1 comp

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args) {
 	int a[] = new int []{1,1,0, 0, 1, 0, 1, 1, 0, 0};
 	printArray(a);
-    for (int i=0; i>a.length; i++){
+    for (int i=0; i<a.length; i++){
         if (a[i]==0) a[i]=1;
         else a[i]=0;
     }
     printArray(a);
     }
     static void printArray (int b[]){
-        for (int i; i>b.length; i++){
+        for (int i=0; i<b.length; i++){
             System.out.print(b[i] + ", ");
         }
         System.out.println("");
